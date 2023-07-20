a01da357a27fbba1bf62b06b26c7b4356773bc59
SMHRD <SMHRD@DESKTOP-D50QT7D>
2023-07-20 16:53:57 +0900

    수정 완

--- scentis/src/main/java/kr/smhrd/controller/MemberController.java
+++ scentis/src/main/java/kr/smhrd/controller/MemberController.java
@@ -24,43 +24,46 @@ public class MemberController {
 	// 메인화면 이동
 	@RequestMapping("/")
 	public String goMain() {
-		return "MainPage";	}
+		return "MainPage";
+	}
 
 	// 로그인 페이지로 이동
 	@RequestMapping("/Login")
 	public String goLogin() {
-		return "LoginPage";	}
+		return "LoginPage";
+	}
 
 	// 음악으로 향수 추천받기 페이지 이동
 	@RequestMapping("/goMusicPerfume")
 	public String goMusicPerfume(Model model) {
 		ArrayList<Perfume> TrendP = mapper.TrendP();
 		model.addAttribute("TrendP", TrendP);
-		return "MusicPerfume";	}
+		return "MusicPerfume";
+	}
 
 	// 향수결과 페이지 이동
 	@PostMapping("/ResultPerfume")
 	public String ResultPerfume() {
-		return "MusicPerfume";	}
+		return "MusicPerfume";
+	}
 
 	// 로그페이지 이동
-		@RequestMapping("/goLogPage")
-		public String goLogPage(Member user, Model model) {
-			if (user.getID() == null) {
-				// 로그인이 되어있지 않다면 로그인 페이지로.
-				return "LoginPage"; 
-			}
-				// 회원의 MyLog + 향수cnt 기준 best top3
-				ArrayList<MyLog> Mylog = mapper.LogCheck(user);
-				model.addAttribute("Mylog", Mylog);
-				ArrayList<Perfume> TrendP = mapper.TrendP();
-				model.addAttribute("TrendP", TrendP);
-
-			// 로그인이 되어있다면 요청대로 로그 페이지로 이동
-			return "LogPage";
+	@RequestMapping("/goLogPage")
+	public String goLogPage(Member user, Model model) {
+		if (user.getID() == null) {
+			// 로그인이 되어있지 않다면 로그인 페이지로.
+			return "LoginPage";
 		}
-		
-		
+		// 회원의 MyLog + 향수cnt 기준 best top3
+		ArrayList<MyLog> Mylog = mapper.LogCheck(user);
+		model.addAttribute("Mylog", Mylog);
+		ArrayList<Perfume> TrendP = mapper.TrendP();
+		model.addAttribute("TrendP", TrendP);
+
+		// 로그인이 되어있다면 요청대로 로그 페이지로 이동
+		return "LogPage";
+	}
+
 	// 향선택 향수 찾기 페이지 이동
 	@RequestMapping("/MixP")
 	public String MixP(Model model) {
@@ -71,10 +74,16 @@ public class MemberController {
 		Pdata.addAll(0, Top);
 		Pdata.addAll(1, Mid);
 		Pdata.addAll(2, Base);
-		model.addAttribute("Pdata",Pdata);
+		model.addAttribute("Pdata", Pdata);
 		return "mixPerfume";
 	}
-	
+
+	// 로그인 페이지로 이동
+	@RequestMapping("/AllP")
+	public String goAllP() {
+		return "AllPerfume";
+	}
+
 	// 로그인
 	@PostMapping("/Login")
 	public String Login(Member member, HttpSession session) {
