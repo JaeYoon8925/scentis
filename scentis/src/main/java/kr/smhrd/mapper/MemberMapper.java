ff07cc1a720f2d2ff75decf00942cf2d3ee7e086
smhrd <smhrd@DESKTOP-1A5AI0F>
2023-07-21 10:50:06 +0900

    내 이전 검색 로그들 불러오기 기능 구현. LogLoad, LogLoadPInfo

--- scentis/src/main/java/kr/smhrd/mapper/MemberMapper.java
+++ scentis/src/main/java/kr/smhrd/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package kr.smhrd.mapper;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -23,16 +24,22 @@ public interface MemberMapper {
 	@Select("SELECT * FROM T_MEMBER WHERE ID=#{ID}")
 	public Member idCheck(String id);
 	
-	// 로그보기
+	// 로그보기, 아래 LogLoad와 LogLoadPInfo 로 기능 구현되서 미사용
 	public ArrayList<MyLog> LogCheck(Member user);
+	
+	// 로그인된 계정의 모든 로그 정보 불러오기
+	public ArrayList<MyLog> LogLoad(String id);
+	
+	// 각 로그별 향수 1 2 3 의 정보 불러오기
+	public ArrayList<Perfume> LogLoadPInfo(@Param("num1") int num1, @Param("num2") int num2, @Param("num3") int num3);
 	
 	// random perfume 하나씩 가져오기
 	@Select("SELECT * FROM T_PERFUME ORDER BY RAND() LIMIT 1")
 	public Perfume RecP();
 	
-   // 향수 모두 가져오기
-   @Select("SELECT * FROM T_PERFUME WHERE P_BRAND=#{name}")
-   public ArrayList<Perfume> AllP(String name);
+    // 향수 모두 가져오기
+    @Select("SELECT * FROM T_PERFUME WHERE P_BRAND=#{name}")
+    public ArrayList<Perfume> AllP(String name);
 	
 	// top,mid,base 가져오기
 	@Select("SELECT DISTINCT P_TOP FROM T_PERFUME")
@@ -46,6 +53,6 @@ public interface MemberMapper {
 	@Select("SELECT * FROM T_PERFUME WHERE P_TYPE = #{P_TYPE} ORDER BY RAND() LIMIT 3")
 	public ArrayList<Perfume> MatchP(MyLog log);
 	
-	// 로그저장
-	public void saveLog(MyLog log);
+//	// 로그저장
+//	public void saveLog(MyLog log);
 }
